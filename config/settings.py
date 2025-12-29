4fbd7edeb2dc900efd5d5f6c49de044a988532b8
Emmanuel Odongo <odiwourmanu123@gmail.com>
2025-12-29 12:16:45 +0300

    Password Reset

--- config/settings.py
+++ config/settings.py
@@ -155,12 +155,12 @@ SESSION_SAVE_EVERY_REQUEST = True
 # Email Configuration (for password reset)  Development
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'  
 # For production:
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = 'smtp.gmail.com'
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 # REST Framework Configuration
 REST_FRAMEWORK = {
--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,7 @@ from django.conf import settings
 from django.views.generic import RedirectView
 from django.templatetags.static import static as static_url
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 # Customize Django Admin site branding
 admin.site.site_header = "UrbanDream Backend"
@@ -27,6 +28,16 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
+
+    # Password Reset
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password_reset.html',
+        email_template_name='password_reset_email.html',
+        subject_template_name='password_reset_subject.txt'
+    ), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
     
     # ==================== STORE SWITCHING ================
     path('api/store/switch/', views.switch_store, name='switch_store'),
