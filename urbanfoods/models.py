0fb7d9522df782aba1fa39949fee589ff97c9a7e
Emmanuel Odongo <odiwourmanu123@gmail.com>
2026-01-07 08:32:45 +0300

    Production Database Setup

--- urbanfoods/models.py
+++ urbanfoods/models.py
@@ -127,6 +127,7 @@ class Order(models.Model):
     ]
     
     order_number = models.CharField(max_length=20, unique=True, editable=False)
+    is_test_order = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
     
